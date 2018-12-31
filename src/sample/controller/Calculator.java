9eddc166f0d3feb1e3cf1cbc45f4a3bc46745dd9
Krish <krishkrash2305@gmail.com>
2019-01-01 00:41:03 +0800

    Use double brace initializer for maps

--- src/sample/controller/Calculator.java
+++ src/sample/controller/Calculator.java
@@ -7,6 +7,8 @@ import javafx.scene.control.TextField;
 import java.util.HashMap;
 import java.util.Map;
 
+import static javax.swing.UIManager.put;
+
 /**
  * Class created by Krish
  */
@@ -142,49 +144,51 @@ public class Calculator {
     private boolean isSecond = false;
     private boolean isHyp = false;
 
-    private Map<Button, String> numbers = new HashMap<>();
-    private Map<Button, String> operations = new HashMap<>();
-    private Map<Button, String[]> multiple = new HashMap<>();
-    private Map<Button, String[]> trigonometry = new HashMap<>();
+    private Map<Button, String> numbers = new HashMap<>() {{
+        put(zero, "0");
+        put(one, "1");
+        put(two, "2");
+        put(three, "3");
+        put(four, "4");
+        put(five, "5");
+        put(six, "6");
+        put(seven, "7");
+        put(eight, "8");
+        put(nine, "9");
+        put(leftParenthesis, "(");
+        put(rightParenthesis, ")");
+    }};
+
+    private Map<Button, String> operations = new HashMap<>() {{
+        put(plus, "+");
+        put(minus, "-");
+        put(multiply, "*");
+        put(divide, "÷");
+        put(negativeOne, "⁻¹");
+        put(scientific, "*10ⁿ");
+    }};
+    
+    private Map<Button, String[]> multiple = new HashMap<>() {{
+        put(decimal, new String[]{".", ","});
+        put(negative, new String[]{"-", "ans"});
+        put(log, new String[]{"log(", "10ˣ"});
+        put(ln, new String[]{"ln(", "eˣ"});
+        put(exponent, new String[]{"ⁿ", "ⁿ√"});
+        put(square, new String[]{"²", "√"});
+    }};
+    
+    private Map<Button, String[]> trigonometry = new HashMap<>() {{
+        put(sin, new String[]{"sin(", "sin⁻¹(", "sinh(", "sinh⁻¹("});
+        put(cos, new String[]{"cos(", "cos⁻¹(", "cosh(", "cosh⁻¹("});
+        put(tan, new String[]{"tan(", "tan⁻¹(", "tanh(", "tanh⁻¹("});
+    }};
 
     @FXML
     void initialize() {
-        numbers.put(zero, "0");
-        numbers.put(one, "1");
-        numbers.put(two, "2");
-        numbers.put(three, "3");
-        numbers.put(four, "4");
-        numbers.put(five, "5");
-        numbers.put(six, "6");
-        numbers.put(seven, "7");
-        numbers.put(eight, "8");
-        numbers.put(nine, "9");
-        numbers.put(leftParenthesis, "(");
-        numbers.put(rightParenthesis, ")");
-
-        operations.put(plus, "+");
-        operations.put(minus, "-");
-        operations.put(multiply, "*");
-        operations.put(divide, "÷");
-        operations.put(negativeOne, "⁻¹");
-        operations.put(scientific, "*10ⁿ");
-
-        multiple.put(decimal, new String[]{".", ","});
-        multiple.put(negative, new String[]{"-", "ans"});
-        multiple.put(log, new String[]{"log(", "10ˣ"});
-        multiple.put(ln, new String[]{"ln(", "eˣ"});
-        multiple.put(exponent, new String[]{"ⁿ", "ⁿ√"});
-        multiple.put(square, new String[]{"²", "√"});
-
-        trigonometry.put(sin, new String[]{"sin(", "sin⁻¹(", "sinh(", "sinh⁻¹("});
-        trigonometry.put(cos, new String[]{"cos(", "cos⁻¹(", "cosh(", "cosh⁻¹("});
-        trigonometry.put(tan, new String[]{"tan(", "tan⁻¹(", "tanh(", "tanh⁻¹("});
 
         on.setOnAction(event -> {
             if (isSecond) System.exit(0);
         });
-        second.setOnAction(event -> isSecond = true);
-        enter.setOnAction(event -> parse());
         pi.setOnAction(event -> {
             if (isSecond) isHyp = true;
             else display.setText(display.getText() + "π");
@@ -193,17 +197,10 @@ public class Calculator {
             if (!display.getText().isEmpty()) display.setText(display.getText(0, display.getText().length() - 1));
         });
         clear.setOnAction(event -> display.setText(""));
+        second.setOnAction(event -> isSecond = true);
+        enter.setOnAction(event -> parse());
 
-        /* todo
-        fraction.setOnAction(event -> output = "");
-        table.setOnAction(event -> output = "");
-        data.setOnAction(event -> output = "");
-        prb.setOnAction(event -> output = "");
-        xyz.setOnAction(event -> output = "");
-        sto.setOnAction(event -> output = "");
-        toggle.setOnAction(event -> output = "");
-        mode.setOnAction(event -> output = "");
-        */
+        // todo: fraction, table, data, prb, xyz, sto, toggle, mode
 
         numbers.forEach((k, v) -> k.setOnAction(event -> {
             display.setText(display.getText() + v);
